22ab932fada59be4d4ca896eb6d041ef795ac5e2
DT6 <dt6cgsg@gmail.com>
2019-11-15 12:46:48 +0300

    Added myself to facts.py

--- practice02/facts.py
+++ practice02/facts.py
@@ -7,6 +7,8 @@ Person = collections.namedtuple('Person', ['github_login', 'first_name', 'last_n
 people = [
     Person(github_login='example', first_name='Name', last_name='Surname',
            facts=['Fact-right.', 'Fact-write.', 'Fact-right.']),
+    Person(github_login='DT6A', first_name='Denis', last_name='Tarasov',
+           facts=['Kek.', 'Lul.', 'False.'])
 ]
 
 answers = {}
